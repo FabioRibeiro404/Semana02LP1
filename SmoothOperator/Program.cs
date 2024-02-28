0bb02be766efbdf90f85b8e8db38b0578b059008
Fábio Ribeiro <fabioribeiro.info@gmail.com>
2024-02-28 17:58:34 +0000

    Add Division, Shift Left and Xor operations

--- SmoothOperator/Program.cs
+++ SmoothOperator/Program.cs
@@ -6,6 +6,7 @@ namespace SmoothOperator
     {
         static void Main(string[] args)
         {
+
             Console.WriteLine("Insert a non-negative number:\n");
             string str = Console.ReadLine();
             byte n = byte.Parse(str);
@@ -13,6 +14,12 @@ namespace SmoothOperator
             Console.WriteLine(n);
             n++;
             Console.WriteLine(n);
+            int div = n / 2;
+            Console.WriteLine(div);
+            int bit = n << 3;
+            Console.WriteLine(bit);
+            int xor = n ^ 5;
+            Console.WriteLine(xor);
         }
     }
 }
