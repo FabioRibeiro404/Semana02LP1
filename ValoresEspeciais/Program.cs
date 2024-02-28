b808e90e379af8d9f6364d3bd703c2fb637e3397
Fábio Ribeiro <fabioribeiro.info@gmail.com>
2024-02-28 18:34:10 +0000

    Add All Max and Min to all types

---
+++ ValoresEspeciais/Program.cs
@@ -0,0 +1,65 @@
+﻿using System;
+
+namespace ValoresEspeciais
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            sbyte num1 = sbyte.MaxValue;
+            sbyte num2 = sbyte.MinValue;
+
+            byte num3 = byte.MaxValue;
+            byte num4 = byte.MinValue;
+
+            ushort num5 = ushort.MaxValue;
+            ushort num6 = ushort.MinValue;
+
+            short num7 = short.MaxValue;
+            short num8 = short.MinValue;
+
+            uint num9 = uint.MaxValue;
+            uint num10 = uint.MinValue;
+
+            int num11 = int.MaxValue;
+            int num12 = int.MinValue;
+
+            ulong num13 = ulong.MaxValue;
+            ulong num14 = ulong.MinValue;
+
+            long num15 = long.MaxValue;
+            long num16 = long.MinValue;
+
+            float num17 = float.MaxValue;
+            float num18 = float.MinValue;
+
+            double num19 = double.MaxValue;
+            double num20 = double.MinValue;
+
+            Console.WriteLine($"Max values:");
+            Console.WriteLine($"sbyte:{num1}");
+            Console.WriteLine($"byte:{num3}");
+            Console.WriteLine($"ushort:{num5}");
+            Console.WriteLine($"short:{num7}");
+            Console.WriteLine($"uint:{num9}");
+            Console.WriteLine($"int:{num11}");
+            Console.WriteLine($"ulong:{num13}");
+            Console.WriteLine($"long:{num15}");
+            Console.WriteLine($"float:{num17}");
+            Console.WriteLine($"sbyte:{num19}\n");
+
+            Console.WriteLine("Min values:");
+            Console.WriteLine($"sbyte:{num2}");
+            Console.WriteLine($"byte:{num4}");
+            Console.WriteLine($"ushort:{num6}");
+            Console.WriteLine($"short:{num8}");
+            Console.WriteLine($"uint:{num10}");
+            Console.WriteLine($"int:{num12}");
+            Console.WriteLine($"ulong:{num14}");
+            Console.WriteLine($"long:{num16}");
+            Console.WriteLine($"float:{num18}");
+            Console.WriteLine($"sbyte:{num20}");
+
+        }
+    }
+}
