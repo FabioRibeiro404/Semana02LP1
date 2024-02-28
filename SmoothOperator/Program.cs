d49f2d9be11bd487a31fc5b862c4ec2dc7bd2bf2
Fábio Ribeiro <fabioribeiro.info@gmail.com>
2024-02-28 08:32:27 +0000

    Add Number request and number increment and decrease

---
+++ SmoothOperator/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace SmoothOperator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert a non-negative number:\n");
+            string str = Console.ReadLine();
+            byte n = byte.Parse(str);
+            n--;
+            Console.WriteLine(n);
+            n++;
+            Console.WriteLine(n);
+        }
+    }
+}
