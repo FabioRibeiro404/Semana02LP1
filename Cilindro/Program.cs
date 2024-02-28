7e05bbaf883108f2eabdb2bc55887adc6bb023f9
Fábio Ribeiro <fabioribeiro.info@gmail.com>
2024-02-28 18:06:37 +0000

    Add Asking dimensions

---
+++ Cilindro/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Cilindro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Insert height:   ");
+            string strH = Console.ReadLine();
+            float height = float.Parse(strH);
+
+            Console.WriteLine("Insert radius:   ");
+            string strR = Console.ReadLine();
+            float radius = float.Parse(strR);
+
+            
+        }
+    }
+}
