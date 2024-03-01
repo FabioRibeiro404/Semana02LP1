533dbea7a8980326c2e8c268f19a80bc60e59ba4
Fábio Ribeiro <fabioribeiro.info@gmail.com>
2024-03-01 13:23:38 +0000

    Add Number verification

---
+++ MasterandPlayer/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MasterandPlayer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Number Master insert number: ");
+            string str = Console.ReadLine();
+            int numMaster = int.Parse(str);
+
+            if (numMaster > 0 && numMaster < 100)
+            {
+                for (int i = 0; i <= 100; i++)
+                {
+                    Console.WriteLine("");
+                }
+                
+            }
+            else
+            {
+                Console.WriteLine("Invalid number!");
+            }
+        }
+    }
+}
